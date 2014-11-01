7fc0c8d4d5beb0632bcf310c68056898aaeb86f3
Simon Sapin <simon.sapin@exyr.org>
2014-10-31 22:42:19 -0700

    Add README.

---
+++ README.rst
@@ -0,0 +1,7 @@
+azureblur
+#########
+
+The `triple box blur <http://dbaron.org/log/20110225-blur-radius>`_
+implementation from Firefox’s
+`moz2d/Azure <https://wiki.mozilla.org/Platform/GFX/Moz2D>`_,
+with Python bindings.
--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+import os
+import io
 from setuptools import setup
 
 setup(
@@ -5,6 +7,11 @@ setup(
     version='0.1',
     url='https://github.com/SimonSapin/azureblur',
     license='MPL2',
+    description='The triple box blur implementation from Firefox’s moz2d/Azure, with Python bindings.',
+    long_description=io.open(
+        os.path.join(os.path.dirname(__file__), 'README.rst'),
+        encoding='utf-8',
+    ).read(),
     install_requires=['cffi'],
     packages=['azureblur'],
     package_data={'azureblur': ['src/*', 'moz2d/*.*', 'moz2d/*/*']},
