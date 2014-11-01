b6ccfca63daf01187ee6ff2b4ce03542ffe4c1f1
Simon Sapin <simon.sapin@exyr.org>
2014-11-01 12:38:21 -0700

    Build C++ code at install time rather than first import.

--- setup.py
+++ setup.py
@@ -2,6 +2,20 @@
 import os
 import io
 from setuptools import setup
+from distutils.command.build import build
+
+
+class cffi_build(build):
+    """
+    This is a shameful hack to ensure that cffi is present
+    when we specify ext_modules.
+    We can't do this eagerly because setup_requires hasn't run yet.
+    """
+    def finalize_options(self):
+        from azureblur import ffi
+        self.distribution.ext_modules = [ffi.verifier.get_extension()]
+        build.finalize_options(self)
+
 
 setup(
     name='azureblur',
@@ -16,6 +30,9 @@ setup(
         encoding='utf-8',
     ).read(),
     install_requires=['cffi'],
+    setup_requires=['cffi'],
     packages=['azureblur'],
     package_data={'azureblur': ['src/*', 'moz2d/*.*', 'moz2d/*/*']},
+    zip_safe=False,
+    cmdclass={'build': cffi_build},
 )
